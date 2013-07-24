1b2fa26528b8f2383b6ab7a85008144adb92f2ca
vivks <vivekkumarsingh@gmail.com>
2013-07-24 15:46:37 -0400

    Update EmageStream.java

--- backend/EmageStream.java
+++ backend/EmageStream.java
@@ -9,6 +9,7 @@ public class EmageStream {
 
 	/*
 	 * TODO: Method to get the next emage in the queue...this class might need some re-thinking
+	 *vks: logically or progrmaatically? but sure, please bring it up in the next meeting?
 	 */
 	public Emage getNextEmage() {
 		return null;
