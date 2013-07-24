569c78e3257af9d3c36dc035ab7b8c860a951404
vivks <vivekkumarsingh@gmail.com>
2013-07-24 15:40:51 -0400

    Update Emage.java
    Added some comments to Pat's questions.

--- backend/Emage.java
+++ backend/Emage.java
@@ -3,11 +3,16 @@ package backend;
 public class Emage {
 
 	//TODO: is there a better datastructure for this? 
+	// vks: it could simply be double valueGrid[][]
+	//The x y coordindates are implicit based on the meta-data
+	
 	STTPoint[][] valueGrid;
 	/*
 	 * TODO: I think we need more params here but I'm not sure how to store them...
 	 * Maybe we should just pass the instance of WrapperParams and EmageParams to the Emage itself?
-	 * will that be a probably when building an emage from the DB though?
+	 * will that be a probably (problem?)when building an emage from the DB though?
+	 *vks: Yes, I pointer to WrapperParams might be good. Get/Put Emage in DB can also be updated to have such a 
+	 * pointer
 	 */
 	
 	/**
@@ -26,6 +31,7 @@ public class Emage {
 	}
 	
 	//TODO: Should value be a double?
+	//vks: yes, let's do double.
 	public void setPoint(int xcoord, int ycoord, int value) {
 
 	}
