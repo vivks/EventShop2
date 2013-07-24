0000395034e3f04bf2165fcea2093d35fe42b56d
vivks <vivekkumarsingh@gmail.com>
2013-07-24 15:42:48 -0400

    Update EmageBuilder.java

--- backend/EmageBuilder.java
+++ backend/EmageBuilder.java
@@ -5,6 +5,8 @@ public class EmageBuilder {
 	/*
 	 * TODO: This definitely needs to be better, probably an enum or something an actual
 	 * operator type. By operator I mean how are we aggregating the points into their buckets
+	 * vks: Yes, enum is good.
+	 * vks: how does this class access WrapperParams? or the emageParams?
 	 */
 	private String operator;
 
